abe03aa50a78baced7e9e1ba59c917390bd5b8fd
jenkins <sohalloran@salesforce.com>
2015-12-11 01:58:23 +0000

    Updated by Jenkins-DEVOPS/Full Backup - sfdc-prod-216

---
+++ src/aura/CORE_WP_Transport/CORE_WP_TransportController.js
@@ -0,0 +1,28 @@
+({
+    doInit : function(component, event, helper) {         
+        var workPlaceId=component.get("v.workplaceLocationId");
+        var action = component.get("c.getTransportDetails");
+        action.setParams({
+            "workPlaceId": workPlaceId
+        });
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                if(response.getReturnValue()!=''){
+                    var str = response.getReturnValue();
+                    var res = str.replace(/http:,mailto/g, "tel");
+                    component.set("v.transport",res);                    
+                }
+                else {
+                    component.set("v.noDataErrorMessage", true);
+                }
+            }                               
+        });
+        $A.enqueueAction(action);
+    },
+    gotoPreviousPage: function(component, event, helper) {         
+        var workplaceLocationId=component.get("v.workplaceLocationId");
+        var selectEvent = $A.get("e.c:CORE_WP_WorkplaceEvent");
+        selectEvent.setParams({"selectedWrokplaceId": workplaceLocationId,"displayWorkplaceHome":true,"WorkplacePannelType":"CORE_WP_WorkplaceHome"}).fire();
+    },
+})
