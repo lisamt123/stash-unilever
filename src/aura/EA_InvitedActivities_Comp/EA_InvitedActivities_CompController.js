75c7e9db08b0e0c5feba6f40a9e75fc05bc72d80
Nilesh Mane <M1023822@mindtree.com>
2016-05-16 11:02:59 +0530

    Everyday Action Prod Release 1.2 Changes

--- src/aura/EA_InvitedActivities_Comp/EA_InvitedActivities_CompController.js
+++ src/aura/EA_InvitedActivities_Comp/EA_InvitedActivities_CompController.js
@@ -1,9 +1,24 @@
 ({
 	gotoDetail : function(component, event, helper) {
-       var actvity=component.get("v.activity");
+        //helper.scrollToLocation(component, "top"); 
+        var actvity=component.get("v.activity");
         var id=actvity.acivityId; 
         var detailpageEvent=$A.get("e.c:EA_Detailpage_Event");
         detailpageEvent.setParams({"actvityid":id});
         detailpageEvent.fire();
 	},
+    
+     gotoInvitation :function(component, event, helper) {
+         //helper.scrollToLocation(component, "top");
+        var member=component.get("v.activity");       
+        var feedbackevent=$A.get("e.c:EA_Accept_Event");
+        feedbackevent.setParams({"activityId":member.acivityId,"teamId":member.member_Id});
+        feedbackevent.fire();
+    },
+    
+    myAction :function(component, event, helper){
+        var act=component.get("v.activity");
+        
+        
+    }
 })
