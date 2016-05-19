fb94a0c6400abd35c84a9d6da640c11ca803bcb3
unknown <511838@PC265666.cts.com>
2016-05-19 16:32:43 +0530

    Synced from 3b364e02355 till 1c8fff64488

--- client/IPMApp/IPM_Resource/js/IPM_SecEditor.js
+++ client/IPMApp/IPM_Resource/js/IPM_SecEditor.js
@@ -346,13 +346,11 @@ jq(function(){
     }
 });
     
-function unloadPage()
+function unloadPageseceditor()
 { 
-if(ckedited === true|| inputchanged === true)
+if((ckedited === true || inputchanged === true) && editorunsaved === true)
 {
-    if(editorunsaved){
         return IPMAppSE.wmessage;            
-    }
 }
 } 
 
@@ -360,7 +358,7 @@ function saveChanges(){
 editorunsaved = false;
 }
 
-window.onbeforeunload = unloadPage;
+window.onbeforeunload = unloadPageseceditor;
 
 /* Below code is to redirect to a page on cancel*/
 function gotoPageNew() {
