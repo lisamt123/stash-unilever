c562a1081dc27f2aefbf97b1760c51d3bea3e7ad
Shruti Suman Mishra <M1020266@mindtree.com>
2016-06-14 17:48:27 +0530

    Pushing Sprint-2 changes

---
+++ src/aura/MB_SpendSummary_Details/MB_SpendSummary_DetailsController.js
@@ -0,0 +1,33 @@
+({
+	doinit : function(component, event, helper) {
+        component.set("v.showspinner","true");
+		 var action = component.get("c.getSpendDetailsforChargelist");
+        action.setParams({"deviceId":component.get("v.deviceId"), "month":component.get("v.month"),"usagetype" : component.get("v.usageType")});
+        action.setCallback(this, function(response){
+            var state = response.getState();  
+            if (component.isValid() && state === "SUCCESS") {
+                var res= response.getReturnValue(); 
+                component.set("v.AllData",res); 
+            } 
+        });
+        $A.enqueueAction(action);
+        /*------------------------------------------------------------*/
+        var action1 = component.get("c.getUsageTotalAmount");
+        action1.setParams({"deviceId":component.get("v.deviceId"), "month":component.get("v.month"),"usagetype" : component.get("v.usageType")});
+        action1.setCallback(this, function(response){
+            var state = response.getState(); 
+            if (component.isValid() && state === "SUCCESS") {
+                var res1= response.getReturnValue();  
+             //alert(res1.UsageCurrentAmount);
+             component.set("v.AllHeadData",res1);
+            } 
+            component.set("v.showspinner","false");
+        });
+        $A.enqueueAction(action1); 
+	},
+    gotoSummarySpend:function(component, event, helper) {
+    var summaryEvent=$A.get("e.c:MB_SummarySpend_Event"); 
+        summaryEvent.setParams({"month":component.get("v.month"),"deviceId":component.get("v.deviceId"),"deviceName":component.get("v.deviceName")}).fire();
+
+    }
+})
---
+++ src/aura/MB_Usage_Comp/MB_Usage_CompController.js
@@ -0,0 +1,41 @@
+({
+	doInit : function(component, event, helper) { 
+       var action = component.get("c.getUsageDetails");
+       action.setParams({ month : component.get("v.CurrentMonth")});
+       action.setCallback(this, function(response){
+           var state = response.getState();
+           console.log("result"+response.getReturnValue());
+           console.log();
+            if (component.isValid() && state === "SUCCESS" ) {
+                res= response.getReturnValue(); 
+                 
+                if(res !==null ){
+                 
+                    component.set("v.usageData",res);
+                    var totalUsage=Number(res.totalUsage).toFixed(2);
+                     component.set("v.totalSpend",totalUsage);
+                    component.set("v.showDetails",true);
+                    component.set("v.userName",res.userName);
+                }
+                
+                   if(res ==null ){
+                    console.log("coming in else part");
+                    var action1=component.get("c.getUserName");
+                    action1.setCallback(this, function(response) {
+                       var state = response.getState();
+                       if (state === "SUCCESS" && response.getReturnValue()!=='') {
+                          
+                           component.set("v.userName",response.getReturnValue());
+                           component.set("v.showDetails",false);
+                           
+                       }
+                   
+                     });
+                   $A.enqueueAction(action1);  
+                   } 
+             }
+           component.set("v.showspinner","false"); 
+         });
+       $A.enqueueAction(action); 
+    },
+})
