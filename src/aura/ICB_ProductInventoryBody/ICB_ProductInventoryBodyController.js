18815d529623ee82ac7c37f68aeb92c0f6ea1522
carlos.h.lima <carlos.h.lima@MW7VTGXMP0N6HB.dir.svc.accenture.com>
2016-08-30 16:03:33 -0300

    Fixes:. - MobileChannel Community siteDotCom - Aura components improvements - Removed PermissionSet ununsed Objects - Removed ununsed Fields from Contact

--- src/aura/ICB_ProductInventoryBody/ICB_ProductInventoryBodyController.js
+++ src/aura/ICB_ProductInventoryBody/ICB_ProductInventoryBodyController.js
@@ -37,7 +37,7 @@
         var index = selectedItem.dataset.record; // Get its value i.e. the index
         var list = component.get("v.Inventory");
         var selectedStore = component.get("v.Inventory")[index];
-        selectedStore.ICB_Quantity_ToBe_Updated__c +=1;
+        selectedStore.ICB_Quantity_Unit__c +=1;
         
         component.set("v.Inventory",list);
         
@@ -50,8 +50,8 @@
         console.log("entrou aki Index: " + index);
         var list = component.get("v.Inventory");
         var selectedStore = component.get("v.Inventory")[index];
-        if (selectedStore.ICB_Quantity_ToBe_Updated__c > 0){
-            selectedStore.ICB_Quantity_ToBe_Updated__c -=1;
+        if (selectedStore.ICB_Quantity_Unit__c > 0){
+            selectedStore.ICB_Quantity_Unit__c -=1;
         }
         component.set("v.Inventory",list);
     },
@@ -59,5 +59,27 @@
     whatButton : function(component, event, helper){
         var whichOne = event.getSource().getLocalId();
         console.log(whichOne);    
-    }
+    },
+    
+    // Updated the inventory operator based a list line item list.
+    updateList : function(component, event, helper){
+        console.log("Entering <updateList>");
+    	var action = component.get("c.updateOperatorInventory");
+        var inventoryToBeUpdate = component.get('v.Inventory');
+        console.log("inventoryToBeUpdate <List>"+ inventoryToBeUpdate);
+        var listJSON=JSON.stringify(inventoryToBeUpdate);
+        console.log("listJSON"+ listJSON);
+        action.setParams({ "listJson" : listJSON });
+        
+        // Add callback behavior for when response is received
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            
+        });
+        
+        // Send action off to be executed
+        $A.enqueueAction(action);
+        location.reload(true);
+        console.log("Exit <updateList>");
+	}
 })
