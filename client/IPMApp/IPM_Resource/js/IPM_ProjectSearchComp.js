f5b7486d296e320303fcea141f740e7189211a87
Anshul Gupta <anshul.gupta4@cognizant.com>
2016-09-16 12:43:28 +0530

    CR510/DEf5096 - Sasank, Santosh

--- client/IPMApp/IPM_Resource/js/IPM_ProjectSearchComp.js
+++ client/IPMApp/IPM_Resource/js/IPM_ProjectSearchComp.js
@@ -1,23 +1,23 @@
 /*  
-*************************************************************************
-*@Description:This script is used for project search page specific interaction
-*@Author: Cognizant
-*@Created Date: 28/05/2015 
-*************************************************************************
-*/ 
+ *************************************************************************
+ *@Description:This script is used for project search page specific interaction
+ *@Author: Cognizant
+ *@Created Date: 28/05/2015 
+ *************************************************************************
+ */
 /* Since we are using global variables we will not be able to create multiple functions. 
 If we split the script to multiple functions the functionality will break. 
 Also variables have to be kept global to make the functionality work. */
- jq(document).ready(function() {                    
     var geoAll;
-    var clusters = [];  
+    var clusters = [];
     var mcos = [];
     var countries = [];
     var unAssigned = 'false';
     var iTORange1 = -1;
-    var brandPositions = [];
+    var brandPositionsnew = [];
     var iTORange2 = -1;
-/* Below function calls the filter function. Filter works based on the value passed to the function. */
+jq(document).ready(function() {
+    /* Below function calls the filter function. Filter works based on the value passed to the function. */
     function filterTLDs() {
         filterTLD(jq('#tldFrom').val(), jq('#tldTo').val());
     }
@@ -25,80 +25,18 @@ Also variables have to be kept global to make the functionality work. */
     function searchProjects() {
         loadProjects(jq('#srchTxt').val());
     }
-    function wrapGeography() {
-        geoAll = jq('#geographyAll').is(":checked");
-        clusters = [];mcos=[];countries=[];
-        /* Below script pushes the value if the condition is true. */
-        jq('.clusterCheck').each(function() {
-            if (jq(this).is(":checked") === true &&  jq(this).val() !== 'Unassigned' ) {
-                clusters.push(jq(this).val());
-            }
-            if(jq(this).is(":checked") === true && jq(this).val() === 'Unassigned'){
-                    unAssigned = 'true';
-            }
-        });
-        /* Below script pushes the value if the condition is true. */
-        jq('.mcoCheck').each(function() {
-            if (jq(this).is(":checked") === true) {
-                mcos.push(jq(this).val());
-            }
-        });
-        /* Below script pushes the value if the condition is true. */
-        jq('.countryCheck').each(function() {
-            if (jq(this).is(":checked") === true) {
-                countries.push(jq(this).val());
-            }
-        });
-    }
+    wrapGeography();
     /* Below works on click event. It calls another function. */
-    jq("#applyFilter").on("click", function() { 
+    jq("#applyFilter").on("click", function() {
         applyFilter();
     });
-    function applyFilter() {
-            var checkedPhases = [];
-            var checkedProjectTypes = [];
-            jq('.phase').each(function() {
-            /* Below script pushes the value if the condition is true. */
-                if (jq(this).is(":checked") ){
-                    checkedPhases.push(jq(this).val());
-                }
-            });
-            /* Below script pushes the value if the condition is true. */
-            jq('.projectType').each(function() {
-                 if (jq(this).is(":checked")){
-                 checkedProjectTypes.push(jq(this).val());
-             }
-            });
-            wrapGeography();                
-            if (jq('.iTORange1').val() !== '') {
-                iTORange1 = jq('.iTORange1').val();
-            }
-            if (jq('.iTORange2').val() !== '') {
-                iTORange2 = jq('.iTORange2').val();
-            }
-            applyFltr(checkedPhases.toString(), jq('.tldFrom').val(), jq('.tldTo').val(), jq('.my').is(":checked"), jq('.aPro').is(":checked"), geoAll, clusters, mcos, countries.toString(), iTORange1, iTORange2, brandPositions.toString(),checkedProjectTypes.toString(),jq('#ActiveProjects').is(':checked'),jq('#StoppedProjects').is(':checked'),unAssigned);   
-        }
+    dropDownFilter();
     chkAll();
     chkSub();
-/* Below script is for the auto complete functionality. It returns a set of results based on the key words entered by the user. */   
-    jq(".filterBrand").autocomplete({
-        source: brandList,
-        select: function(event, ui) {
-            jq('.filterBrand').html('');
-            if(jq.inArray( ui.item.value, brandPositions) === -1) {
-                brandPositions.push(ui.item.value);
-                return jq('<li class="brndFilter"></li>').append('<div class="selectedBrand">' + ui.item.value + '</div>').appendTo(jq('ul.filterBrandList'));
-            }
-        }
-    });
-    /* Below works on click event. It removes the list when clicked on it */
-     jq(".filterBrandList").on("click", ".brndFilter", function() {
-        brandPositions.splice( jq.inArray(jq(this).text(), brandPositions), 1 );
-        jq(this).remove();
-    });  
+   
 });
 
-function chkAll(){
+function chkAll() {
     /* Below script works on click event. When the check all is checked all the child checkboxes will be checked. */
     jq(".docFilter").on("click", ".checkAll", function() {
         if (jq(this).is(":checked")) {
@@ -109,9 +47,9 @@ function chkAll(){
             jq(this).closest("ul").find("input[type=checkbox]:not('[disabled]')").next().removeClass("selected");
         }
     });
-    
+
     /* Below script replace a text with a specific time format. */
-     jq(".dueDate,.comDate").each(function() {
+    jq(".dueDate,.comDate").each(function() {
         jq(this).text(jq(this).text().replace('00:00:00 GMT', ''));
     });
     jq('tr:nth-child(4n),tr:nth-child(4n-1)').addClass('greyColr').removeClass('noBackground');
@@ -124,7 +62,7 @@ function chkAll(){
             return false;
         }
     });
-    
+
     /* Below script works on keypress. When user presses enter key a search function is called */
     jq('#srchBrand').keypress(function(e) {
         if (e.which === 13) {
@@ -134,10 +72,10 @@ function chkAll(){
     });
 }
 
-function chkSub(){
+function chkSub() {
     /* Below script works on click event. It reloads the current page. */
     jq(document).on('click', '#resetFilterBtnNew', function() {
-        location.reload();
+        window.location.href = window.location.href;
     });
     /* Below script works on click event. Based on condition it checks for a specific class in below conditions.
 Also if the checkbox is checked it will check its own related child checkboxes. */
@@ -205,24 +143,123 @@ Also if the checkbox is checked it will check its own related child checkboxes. 
     });
 }
 /* Below function performs unchecking of children checkboxes when the condition is true. */
-    function unCheckChildren(str) {
-        jq('.' + str).each(function() {
-            if (jq(this).val() !== 'all') {
-                jq(this).attr("checked", false);
+function unCheckChildren(str) {
+    jq('.' + str).each(function() {
+        if (jq(this).val() !== 'all') {
+            jq(this).attr("checked", false);
+        }
+    });
+}
+/* Below function performs checking of children checkboxes. */
+function checkChildren(str) {
+    jq('.' + str).each(function() {
+        jq(this).attr("checked", true);
+    });
+}
+/* Below function performs unchecking of parent checkboxes when the condition is true. */
+function unCheckParent(str) {
+    jq('.' + str).each(function() {
+        if (jq(this).val() === 'all') {
+            jq(this).attr("checked", false);
+        }
+    });
+}
+
+
+
+
+function wrapGeography() {
+        geoAll = jq('#geographyAll').is(":checked");
+        clusters = [];
+        mcos = [];
+        countries = [];
+        /* Below script pushes the value if the condition is true. */
+        jq('.clusterCheck').each(function() {
+            if (jq(this).is(":checked") === true && jq(this).val() !== 'Unassigned') {
+                clusters.push(jq(this).val());
+            }
+            if (jq(this).is(":checked") === true && jq(this).val() === 'Unassigned') {
+                unAssigned = 'true';
+            }
+        });
+        /* Below script pushes the value if the condition is true. */
+        jq('.mcoCheck').each(function() {
+            if (jq(this).is(":checked") === true) {
+                mcos.push(jq(this).val());
+            }
+        });
+        /* Below script pushes the value if the condition is true. */
+        jq('.countryCheck').each(function() {
+            if (jq(this).is(":checked") === true) {
+                countries.push(jq(this).val());
             }
         });
     }
-    /* Below function performs checking of children checkboxes. */
-    function checkChildren(str) {
-        jq('.' + str).each(function() {
-            jq(this).attr("checked", true);
-        });
-    }
-    /* Below function performs unchecking of parent checkboxes when the condition is true. */
-    function unCheckParent(str) {
-        jq('.' + str).each(function() {
-            if (jq(this).val() === 'all') {
-                jq(this).attr("checked", false);
+    
+function applyFilter() {
+        var checkedPhases = [];
+        var checkedProjectTypes = [];
+        jq('.phase').each(function() {
+            /* Below script pushes the value if the condition is true. */
+            if (jq(this).is(":checked")) {
+                checkedPhases.push(jq(this).val());
             }
         });
-    }   
+        /* Below script pushes the value if the condition is true. */
+        jq('.projectType').each(function() {
+            if (jq(this).is(":checked")) {
+                checkedProjectTypes.push(jq(this).val());
+            }
+        });
+        wrapGeography();
+        if (jq('.iTORange1').val() !== '') {
+            iTORange1 = jq('.iTORange1').val();
+        }
+        if (jq('.iTORange2').val() !== '') {
+            iTORange2 = jq('.iTORange2').val();
+        }
+        applyFltr(checkedPhases.toString(), jq('.tldFrom').val(), jq('.tldTo').val(), jq('.my').is(":checked"), jq('.aPro').is(":checked"), geoAll, clusters, mcos, countries.toString(), iTORange1, iTORange2, brandPositionsnew.toString(), checkedProjectTypes.toString(), jq('#ActiveProjects').is(':checked'), jq('#StoppedProjects').is(':checked'), unAssigned);
+    }
+    
+function dropDownFilter(){
+jq("div.brandSearchResultsClass").on("click", "div", function(event) {
+        brandsVal = jq(this).text();
+        jq('.filterBrand').val(brandsVal);
+        jq('.brandSearchResultsClass').hide();
+        jq("#brandSearchResults div").remove();
+        jq('<li class="brndFilter"></li>').append('<div class="selectedBrand">' + brandsVal + '</div>').appendTo(jq('ul.filterBrandList'));
+        var seen = {};
+        var i = 0;
+        jq('div.selectedBrand').each(function() {
+            var txt = jq(this).text();
+            if (seen[txt]){
+                jq(this).parent().remove();
+            }else {
+                seen[txt] = true;
+                brandPositionsnew[i] = txt;
+                ++i;
+            }
+        });
+    });
+
+    jq(document).click(function(e) {
+        if (e.target.id != 'brandSearchResults') {
+            jq("#brandSearchResults").hide();
+        }
+    });
+
+    jq('.filterBrand').on('keyup', function() {
+        var txtCount = jq(this).val().length;
+        if (txtCount >= 2) {
+            jq('.brandSearchResultsClass').show();
+        } else {
+            jq('.brandSearchResultsClass').hide();
+        }
+    });
+    
+     /* Below works on click event. It removes the list when clicked on it */
+    jq(".filterBrandList").on("click", ".brndFilter", function() {
+        brandPositionsnew.splice(jq.inArray(jq(this).text(), brandPositionsnew), 1);
+        jq(this).remove();
+    });
+}
