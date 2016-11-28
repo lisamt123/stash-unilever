57f3eccab4ce1988a76221b09cbcd7b2c49e9c88
CORP\emaharan <epsita.maharana@capgemini.com>
2016-11-28 14:03:01 +0530

    changes

--- client/VPM/VPM_ProcurementCentralAdminScript/VPM_ProcurementCentralAdminScript.js
+++ client/VPM/VPM_ProcurementCentralAdminScript/VPM_ProcurementCentralAdminScript.js
@@ -6,7 +6,6 @@
     }
     
      function callChangeApprover(){
-     debugger;
          if($('.errorM3').is(":visible")){
              $('[id*="replaceRender1"],[id*="replaceRender2"]').removeClass('hideDropdown');
              $('select[title="Search scope"]').css('display','none');
