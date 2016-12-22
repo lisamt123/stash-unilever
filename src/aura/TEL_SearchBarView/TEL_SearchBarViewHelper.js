7f7f6d0a2b16cdf0352a8056364a17c495615a4e
djtmaranhao <djtmaranhao@gmail.com>
2016-12-22 09:15:18 -0300

    Telesales - Sprint 1

---
+++ src/aura/TEL_SearchBarView/TEL_SearchBarViewHelper.js
@@ -0,0 +1,38 @@
+({
+    setSearchedProducts : function(component, event) {
+        console.log("Initiating setSearchedProducts ");
+        
+        var searchKey = event.getParam("searchKey");
+        var fullProdList = component.get("v.fullWrapperProductsList");
+        var searchedProdList = new Array();
+        
+        for(var counter = 0; counter < fullProdList.length; counter++) {
+            var prodName = "" + fullProdList[counter].name;
+            //if(prodName.includes(searchKey)) {
+            if(prodName.toLowerCase().indexOf(searchKey.toLowerCase()) != -1) {
+                searchedProdList.push(fullProdList[counter]);
+            }
+        }
+        
+        component.set("v.searchedProductsList", searchedProdList);
+        
+        console.log("Exiting setSearchedProducts ");
+    }, 
+    
+    setAddProductEvent : function(component, event) {
+        console.log("Initiating setAddProductEvent ");
+        
+        var fireEvent = component.getEvent("addProduct");
+        var selectedItem = event.currentTarget;
+        var index = selectedItem.dataset.record;
+        var list = component.get("v.searchedProductsList");
+        
+        fireEvent.setParams({
+            "productSelected" : list[index]
+        });
+        
+        fireEvent.fire();
+        
+        console.log("Exiting setAddProductEvent ");
+    }
+})
