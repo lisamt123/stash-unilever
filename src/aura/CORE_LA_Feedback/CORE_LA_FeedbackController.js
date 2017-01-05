f39374d864304885b67973d06b52dac721280236
Sanjusmita Mohanty <M1029964@mindtree.com>
2017-01-05 17:51:12 +0530

    updated code LA

--- src/aura/CORE_LA_Feedback/CORE_LA_FeedbackController.js
+++ src/aura/CORE_LA_Feedback/CORE_LA_FeedbackController.js
@@ -4,9 +4,11 @@
         selectEvent.setParams({"componentName":"c:CORE_LA_Home"}).fire();
 		
 	},
-    submit:function(component, event, helper) {
-       var selectEvent = $A.get("e.c:CORE_LA_HandleEvent");
-        selectEvent.setParams({"componentName":"c:CORE_LA_Home"}).fire();
+   submit:function(component, event, helper) {
+    var selectEvent = $A.get("e.c:CORE_LA_HandleEvent");
+    selectEvent.setParams({"componentName":"c:CORE_LA_Home"}).fire();
 		
 	},
+   
+   
 })
